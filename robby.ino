6e1a8cbe70d33516554c930938b0a7c1418e205b
fischundfleisch <94743049+fischundfleisch@users.noreply.github.com>
2022-02-04 21:10:43 +0100

    Update robby.ino
    quicksort hinzugefügt

--- robby.ino
+++ robby.ino
@@ -92,6 +92,36 @@ void getDistance_right(){
   Serial.println(duration_right);
 }
 
+void quicksort(int array[], int left, int right) {
+  int i = left;
+  int j = right;
+  int temp = 0;
+  int pivot = array[(left+right)/2];
+
+  while (i<=j) {
+    while (array[i] < pivot) {
+      i++;
+    }
+    while (array[j] > pivot){
+      j--;
+    }
+    if (i<=j) {
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+      i++;
+      j--;
+    }
+  }
+
+  if (left < j) {
+    quicksort(array, left, j);
+  }
+  if (i < right) {
+    quicksort(array, i, right);
+  }
+}
+
 void loop() {
 Serial.println("Distanz Front:");
 getDistance_front();
